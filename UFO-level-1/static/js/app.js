71869418dcf99964f3b55b58fdc758ab58a073c8
Lavanya Ganesh <lavanya.ganesh78@gmail.com>
2020-06-01 23:06:07 -0400

    first push

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,48 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// selecting table tbody
+var tbody = d3.select("tbody");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+function runEnter(){
+
+    // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+    var filterdate = d3.select("#datetime");
+    var filterdatevalue =filterdate.property("value");
+    console.log("value is ",filterdatevalue);
+
+    var filteredtableData = tableData.filter(ufo => ufo.datetime === filterdatevalue);
+
+    console.log(filteredtableData);
+
+   
+
+    // Adding the table data
+    tableData.forEach(function(uforeport)
+    {
+    console.log(uforeport);
+    var myrow = tbody.append('tr');
+    Object.entries(uforeport).forEach(function([key,value]){
+    console.log([key,value]);
+    var cell = myrow.append("td");
+    cell.text(value);
+    });
+    });
+
+
+};
